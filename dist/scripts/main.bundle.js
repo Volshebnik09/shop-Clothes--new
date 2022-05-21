6206e1ad3b1e148ef5943a885eec11d2292277b0
Fridge <shulgin.grigoriy@yandex.ru>
2022-05-22 00:03:05 +0300

    header bug fix

--- dist/scripts/main.bundle.js
+++ dist/scripts/main.bundle.js
@@ -75,19 +75,23 @@
 	    let content = popup.querySelector('.header__popup-menu__content');
 	    let exit = popup.querySelector('.header__popup-menu__close svg');
 	    let open = document.querySelector('.header__popup-button svg')
+	    let body = document.body;
 	    content.addEventListener('click',(e)=>{
 	        e.stopPropagation();
 	    })
 	    exit.addEventListener('click', (e)=>{
 	        e.stopPropagation()
 	        popup.removeAttribute('active')
+	        body.style.overflow = 'auto'
 	    })
 	    open.addEventListener('click', (e)=>{
 	        e.stopPropagation();
 	        popup.setAttribute('active','')
+	        body.style.overflow = 'hidden'
 	    })
 	    popup.addEventListener('click',(e)=>{
 	        popup.removeAttribute('active')
+	        body.style.overflow = 'auto'
 	    })
 
 	    let links = popup.querySelectorAll('#checkForClick')
--- src/components/header__popup-menu/index.js
+++ src/components/header__popup-menu/index.js
@@ -3,19 +3,23 @@ module.exports = function(el = '.header__popup-menu'){
     let content = popup.querySelector('.header__popup-menu__content');
     let exit = popup.querySelector('.header__popup-menu__close svg');
     let open = document.querySelector('.header__popup-button svg')
+    let body = document.body;
     content.addEventListener('click',(e)=>{
         e.stopPropagation();
     })
     exit.addEventListener('click', (e)=>{
         e.stopPropagation()
         popup.removeAttribute('active')
+        body.style.overflow = 'auto'
     })
     open.addEventListener('click', (e)=>{
         e.stopPropagation();
         popup.setAttribute('active','')
+        body.style.overflow = 'hidden'
     })
     popup.addEventListener('click',(e)=>{
         popup.removeAttribute('active')
+        body.style.overflow = 'auto'
     })
 
     let links = popup.querySelectorAll('#checkForClick')
