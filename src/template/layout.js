92dda67e676f29d48d7b5698101487b1b6c89af0
Fridge <shulgin.grigoriy@yandex.ru>
2022-06-10 16:06:27 +0300

    adding meta and  update .gitignore

--- src/template/layout.js
+++ src/template/layout.js
@@ -1,4 +1,24 @@
 var animateHeader = require('../components/header__search/index.js');
 var activateHeaderPopup = require('../components/header__popup-menu/index.js');
 animateHeader(document.querySelector('.header__search'));
-activateHeaderPopup(); 
+activateHeaderPopup();
+
+var meta = document.createElement('meta');
+meta.name = "viewport";
+meta.content = "width=480";
+
+document.body.onresize = () => {
+    addMetaWidth();
+}
+ 
+addMetaWidth = () => {
+    if (window.screen.width <= 480) {
+        document.querySelector('head').append(meta);
+    } else {
+        console.log(123)
+        meta.remove()
+    }
+}
+
+
+addMetaWidth()
