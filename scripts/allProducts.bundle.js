d275868fcf8e9ae34be28e7a90a5232a2ed58914
Nekitech <Nekitech@users.noreply.github.com>
2022-06-30 23:02:46 +0000

    deploy: c8f3825a01ab20112e42df8676f6132090b0a092

--- scripts/allProducts.bundle.js
+++ scripts/allProducts.bundle.js
@@ -9,23 +9,23 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "../src/mixins/card-product/popupProd.js":
+/*!***********************************************!*\
+  !*** ../src/mixins/card-product/popupProd.js ***!
+  \***********************************************/
+/***/ (() => {
+
+eval("var cards = document.querySelectorAll('.card-product');\nvar cardPopups = Array.from(document.querySelectorAll('.card-popup'));\ncards.forEach(function (card, index) {\n  var img_card = card.querySelector('.card-product__img');\n  var close_btn = cardPopups[index].querySelector('.card-popup__close-btn');\n  img_card.addEventListener('click', function (e) {\n    document.body.style.overflow = 'hidden';\n    cardPopups[index].style.display = 'block';\n  });\n  close_btn.addEventListener('click', function (e) {\n    cardPopups[index].style.display = 'none';\n    document.body.style.overflow = 'auto';\n  });\n});\nwindow.addEventListener('click', function (e) {\n  if (e.target.classList.contains('card-popup')) {\n    e.target.style.display = 'none';\n    document.body.style.overflow = 'auto';\n  }\n});\n\n//# sourceURL=webpack:///../src/mixins/card-product/popupProd.js?");
+
+/***/ }),
+
 /***/ "../src/pages/allProducts/allProducts.js":
 /*!***********************************************!*\
   !*** ../src/pages/allProducts/allProducts.js ***!
   \***********************************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("__webpack_require__(/*! ../../sections/allProducts__products/popupProd.js */ \"../src/sections/allProducts__products/popupProd.js\");\n\nvar numbersPages = document.querySelector('.allProducts__products__numbers-pages');\nvar listNumbers = document.querySelectorAll('.allProducts__products__numbers-pages__item');\nnumbersPages.addEventListener('click', function (e) {\n  listNumbers.forEach(function (el) {\n    if (el.classList.contains('allProducts__products__numbers-pages__item__active') && e.target !== el && e.target !== numbersPages) {\n      el.classList.remove('allProducts__products__numbers-pages__item__active');\n    }\n  });\n\n  if (e.target.classList.contains('allProducts__products__numbers-pages__item')) {\n    e.target.classList.add('allProducts__products__numbers-pages__item__active');\n  }\n});\n\n//# sourceURL=webpack:///../src/pages/allProducts/allProducts.js?");
-
-/***/ }),
-
-/***/ "../src/sections/allProducts__products/popupProd.js":
-/*!**********************************************************!*\
-  !*** ../src/sections/allProducts__products/popupProd.js ***!
-  \**********************************************************/
-/***/ (() => {
-
-eval("var cards = document.querySelectorAll('.card-product');\nvar cardPopups = Array.from(document.querySelectorAll('.card-popup'));\ncards.forEach(function (card, index) {\n  var img_card = card.querySelector('.card-product__img');\n  img_card.addEventListener('click', function (e) {\n    cardPopups[index].style.display = 'fixed';\n  });\n});\n\n//# sourceURL=webpack:///../src/sections/allProducts__products/popupProd.js?");
+eval("__webpack_require__(/*! ../../mixins/card-product/popupProd.js */ \"../src/mixins/card-product/popupProd.js\");\n\nvar numbersPages = document.querySelector('.allProducts__products__numbers-pages');\nvar listNumbers = document.querySelectorAll('.allProducts__products__numbers-pages__item');\nnumbersPages.addEventListener('click', function (e) {\n  listNumbers.forEach(function (el) {\n    if (el.classList.contains('allProducts__products__numbers-pages__item__active') && e.target !== el && e.target !== numbersPages) {\n      el.classList.remove('allProducts__products__numbers-pages__item__active');\n    }\n  });\n\n  if (e.target.classList.contains('allProducts__products__numbers-pages__item')) {\n    e.target.classList.add('allProducts__products__numbers-pages__item__active');\n  }\n});\n\n//# sourceURL=webpack:///../src/pages/allProducts/allProducts.js?");
 
 /***/ })
 
